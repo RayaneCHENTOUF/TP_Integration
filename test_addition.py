f765077244f19d3b026778bd1242cf78e26b8a32
RayaneCHENTOUF <rayane.ch2209@gmail.com>
2026-09-07 11:49:40 +0200

    test_unitaire

---
+++ test_addition.py
@@ -0,0 +1,11 @@
+import unittest
+from addition import addition
+
+
+class TestAddition(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(addition(2, 3), 5)
+
+
+if __name__ == "__main__":
+    unittest.main()
